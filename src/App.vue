6da42430a2c8468385a971303ca34b9e8a7bbb08
fly.han <fly.han@dji.com>
2022-01-05 12:13:03 +0800

    init github page

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-</script>
-
 <template>
-  <button>Click</button>
+  <button @click="handleClick">Click</button>
 </template>
 
+<script>
+export default {
+  name: 'App',
+  methods: {
+    handleClick(e){
+      console.log(e)
+    }
+  }
+}
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
